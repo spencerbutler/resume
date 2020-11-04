6650ceedabcd11fba08e29678c6293c04369be38
Spencer Butler <github@crooked.app>
2020-11-04 10:45:10 -0600

    post KW update (#3)

--- convert.sh
+++ convert.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # Spencer Butler <github@crooked.app>
-# Convert SpencerButler.md to SpencerButler.{html,pdf,json}
+# Convert SpencerButler.md to SpencerButler.{html,pdf,txt}
 
 resume="${resume:-"SpencerButler"}"
 
 pandoc -s "${resume}".md -t html5 -o "${resume}".html
-pandoc -s "${resume}".md -t json  -o "${resume}".json
 pandoc -s "${resume}".md -t latex  -o "${resume}".pdf
 
 sed -e 's/^#*[[:space:]]//g' "${resume}".md > "${resume}".txt
